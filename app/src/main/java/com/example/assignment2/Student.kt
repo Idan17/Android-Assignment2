a53d39c4da4cbe42ba320d30828323c209278f17
Idan17 <idanbartov@gmail.com>
2025-01-04 16:48:47 +0200

    delete student class

--- app/src/main/java/com/example/assignment2/Student.kt
+++
@@ -1,8 +0,0 @@
-package com.example.assignment2
-
-data class Student(
-    var id: String,       // ID of the student (editable)
-    var name: String,     // Name of the student (editable)
-    var isChecked: Boolean, // Checkbox status (checked/unchecked)
-    var photoResId: Int   // Resource ID for the student's static photo
-)
