6f253f72ce562c8365430fc0f7b9f72b3d24cbda
Idan17 <idanbartov@gmail.com>
2025-01-04 16:30:17 +0200

    feat: Students list

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.assignment2
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: StudentsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,34 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        recyclerView = findViewById(R.id.recyclerView)
+        val fabAdd = findViewById<FloatingActionButton>(R.id.main_activity_add_button)
+
+        adapter = StudentsAdapter(
+            StudentRepository.getStudents(),
+            onRowClick = { student ->
+                val intent = Intent(this, StudentDetailsActivity::class.java)
+                intent.putExtra("studentId", student.id)
+                startActivity(intent)
+            },
+            onCheckClick = { student ->
+                student.isChecked = !student.isChecked
+                adapter.notifyDataSetChanged()
+            }
+        )
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        fabAdd.setOnClickListener {
+            startActivity(Intent(this, NewStudentActivity::class.java))
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onResume() {
+        super.onResume()
+        adapter.notifyDataSetChanged() // Refresh list when returning to this screen
     }
 }
